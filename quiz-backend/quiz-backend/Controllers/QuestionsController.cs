87f96456ee30f2888eabb2ce2b45304ed5c5c887
Woohoohoohoo! <wisconsinsara@gmail.com>
2024-03-29 13:02:00 -0700

    Need a backend I can hit with Postman

---
+++ quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            return "Pong";
+        }
+    }
+}
