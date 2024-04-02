0dc6b15cb23371a46ffecdb25517d0e9e75e41d6
Woohoohoohoo! <wisconsinsara@gmail.com>
2024-04-01 20:15:42 -0700

    Let's post the user's question to the console.

--- frontend/src/app/question.component.ts
+++ frontend/src/app/question.component.ts
@@ -1,12 +1,19 @@
 import { Component } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule} from '@angular/material/card';
 
 @Component({
     selector: 'question',
     standalone: true,
-    imports: [MatFormFieldModule, MatInputModule],
+    imports: [FormsModule, MatCardModule, MatFormFieldModule, MatInputModule],
     templateUrl: './question.component.html',
 })
 
-export class QuestionComponent { }
+export class QuestionComponent { 
+    question: string = '';
+    post(question: string): void { 
+        console.log(question);
+    }
+}
